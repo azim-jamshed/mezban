10c58c49bcae78b3129c36420dd746a0edbf196d
Azim Jamshed <sadmanazim.bd@gmail.com>
2024-08-21 16:32:44 +0600

    first commit

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+
+function mezban_theme_support(){
+
+
+
+
+}
+
+
+add_action( 'after_setup_theme', 'mezban_theme_support' );
---
+++ header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> >
+	<head>
+		<meta charset="<?php bloginfo( 'charset' ); ?>">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Mezban  - Food & Resturent HTML Template</title>
+
+		<!-- favicon -->
+		<link rel="shortcut icon" href="assets/images/favicon.png" type="image/png">
+		
+        
+        <!-- animate scss -->
+		<link rel="stylesheet" href="assets/css/animate.css">
+		<!-- bootstarp css -->
+		<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+		<!-- icofont -->
+		<link rel="stylesheet" href="assets/css/icofont.min.css">
+		<!-- lightcase css -->
+		<link rel="stylesheet" href="assets/css/lightcase.css">
+		<!-- swiper css -->
+		<link rel="stylesheet" href="assets/css/swiper.min.css">
+		<!-- custom scss -->
+		<link rel="stylesheet" href="assets/css/style.css">
+
+		<?php wp_head(); ?> 
+
+	</head>
+
+	<body>
+
+
+
+
+
